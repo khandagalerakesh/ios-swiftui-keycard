73715bb233cb641e74b509c5cb5b31d2f02f0cdf
Rakesh Gorkhanath Khandagale <khandagale87.rakesh@gmail.com>
2021-01-26 15:53:20 -0500

    xcode project setup. add images to project

---
+++ app-keycard/app-keycard/app_keycardApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_keycardApp.swift
+//  app-keycard
+//
+//  Created by Rakesh Khandagale on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct app_keycardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
