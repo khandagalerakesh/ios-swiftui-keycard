6ebf7be3e6df1b9b87a86cb86353bcd945764915
Rakesh Gorkhanath Khandagale <khandagale87.rakesh@gmail.com>
2021-01-26 16:09:36 -0500

    ui design for front of card complete

--- app-keycard/app-keycard/Screens/CardFrontScreen.swift
+++ app-keycard/app-keycard/Screens/CardFrontScreen.swift
@@ -9,8 +9,62 @@ import SwiftUI
 
 struct CardFrontScreen: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            // Top Section
+            VStack {
+                Rectangle()
+                    .fill(Color(red: 52.0/255.0, green: 52.0/255.0, blue: 52.0/255.0))
+                    .frame(width: 100, height: 15, alignment: .center)
+                    .cornerRadius(10.0)
+                
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .frame(maxHeight: 250)
+                    .clipShape(Circle())
+            }
             .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.red)
+            .edgesIgnoringSafeArea(.all)
+            
+            Spacer()
+            
+            // Bottom Section
+            VStack {
+                HStack {
+                    Text("RAKESH")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text("KHANDAGALE")
+                        .font(.largeTitle)
+                        .fontWeight(.thin)
+                }
+                .padding(.top, 25.0)
+                .padding(.bottom, 10.0)
+                
+                Text("iOS DEVELOPER")
+                    .font(.subheadline)
+                    .fontWeight(.light)
+                
+                Spacer()
+                
+                HStack {
+                    Text("YOUR")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("COMPANY")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.red)
+                }
+                .padding()
+            }
+            .frame(maxWidth: .infinity)
+            .frame(maxHeight: 350)
+            .background(Color.white)
+        }
     }
 }
 
